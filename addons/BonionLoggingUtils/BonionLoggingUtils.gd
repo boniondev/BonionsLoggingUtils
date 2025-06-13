7f8e2a0851f85daf501affffbf3a285cd0ea2b66
Bonion-Laptop <55188452+boniondev@users.noreply.github.com>
2025-06-13 12:17:06 +0200

    Squashed commit of the following:
    commit 6f39a4984ce8bd22ab5e4379d14587dbe4c8de2e
    Author: Bonion-Laptop <55188452+boniondev@users.noreply.github.com>
    Date:   Fri Jun 13 12:09:33 2025 +0200
    
        Even more docs
    
    commit ddd7f121697ab69af4aaa23a86200f5fc639b022
    Author: Bonion-Laptop <55188452+boniondev@users.noreply.github.com>
    Date:   Fri Jun 13 11:48:58 2025 +0200
    
        Create .gdignore
    
    commit 841c25227f27f964a2d938c1a65ddc255c137da9
    Author: Bonion-Laptop <55188452+boniondev@users.noreply.github.com>
    Date:   Fri Jun 13 11:48:50 2025 +0200
    
        Further doc edits
    
    commit b5c507b22418894cd5d5156276038d4937d1fc48
    Author: Bonion-Laptop <55188452+boniondev@users.noreply.github.com>
    Date:   Fri Jun 13 11:39:16 2025 +0200
    
        Minor doc edit
    
    commit 60f3b8d5dbd63000579ce115d41f3a7f86545f84
    Author: Bonion-Laptop <55188452+boniondev@users.noreply.github.com>
    Date:   Fri Jun 13 11:38:56 2025 +0200
    
        Remove debugging stuff
    
    commit 4bf2acb9b36d99803a89d5e5caf908ce799b0637
    Author: Bonion-Laptop <55188452+boniondev@users.noreply.github.com>
    Date:   Fri Jun 13 11:36:16 2025 +0200
    
        Made buffer private, empty buffer after saving
    
    commit 7673da6cab7deb235279061163b529aaed7421d0
    Author: Bonion-Laptop <55188452+boniondev@users.noreply.github.com>
    Date:   Fri Jun 13 11:32:10 2025 +0200
    
        Hide currently unused func
    
    commit ac093c4d2e98e594d93307f5f9310d02b6f8dc70
    Author: Bonion-Laptop <55188452+boniondev@users.noreply.github.com>
    Date:   Fri Jun 13 11:25:49 2025 +0200
    
        Update .gitattributes
    
    commit c5d6e3bf47873db2abba676863a0855d473be876
    Author: Bonion-Laptop <55188452+boniondev@users.noreply.github.com>
    Date:   Fri Jun 13 11:23:03 2025 +0200
    
        Whoops
    
    commit 03443fe2bc07433874eb05d54efa633dfa3b9ade
    Author: Bonion-Laptop <55188452+boniondev@users.noreply.github.com>
    Date:   Fri Jun 13 11:20:44 2025 +0200
    
        Asset Library compliance
    
    commit ca4d6a6fce1a08bd8dfb74be60c88518dc042579
    Merge: 4927078 fa29292
    Author: boniondev <55188452+boniondev@users.noreply.github.com>
    Date:   Tue Jun 10 12:18:01 2025 +0200
    
        Merge pull request #2 from boniondev/var-buffer
    
        Rely on String buffer
    
    commit fa29292ed08563fb93c5e2d4fc446c72ef442728
    Author: Bonion-Laptop <55188452+boniondev@users.noreply.github.com>
    Date:   Tue Jun 10 12:16:01 2025 +0200
    
        Rely on String buffer
    
        FileAccess leaves file open and untouchable

--- addons/BonionLoggingUtils/BonionLoggingUtils.gd
+++ addons/BonionLoggingUtils/BonionLoggingUtils.gd
@@ -1,13 +1,19 @@
 class_name BonionLoggingUtils extends GDScript
 
+## A class used to print logs. Must be instantiated by using [method GDScript.new] to be used.[br]
+## Logs will be stored in user://BonionLoggingUtils_logs/.[br]
+## This class also creates and uses user://BonionLoggingUtils_config.json
+## to determine how many logs it will keep stored before deleting the oldest one
+## and whether to require manual saving or to save upon exit.
+
 const _PERSISTENTPATH  : String = "user://BonionLoggingUtils_config.json"
-var buffer             : String
+var _buffer            : String
 
 func _notification(what: int) -> void:
 	if what == NOTIFICATION_PREDELETE:
-		if buffer != null and _AUTOSAVEONEXIT:
+		if _buffer != null and _buffer != "" and _AUTOSAVEONEXIT:
 			var file = FileAccess.open(_LOGFILESPATH + "latest.log", FileAccess.WRITE_READ)
-			file.store_string(buffer)
+			file.store_string(_buffer)
 			file.close()
 
 func _init() -> void:
@@ -57,8 +63,8 @@ const      _LOGFILESPATH               : String = _LOGFOLDERPATH + "/"
 var        _MAXLOGFILES                : int    = 5
 static var _AUTOSAVEONEXIT             : bool   = true
 
-func setMAXLOGFILES(number : int) -> void:
-	_MAXLOGFILES = number
+#func setMAXLOGFILES(number : int) -> void:
+	#_MAXLOGFILES = number
 
 ## Varying levels of log severities, along with guidelines as to how and when to use them.
 enum LOGSEVERITY {
@@ -70,7 +76,8 @@ enum LOGSEVERITY {
 	WARNING,
 	## Events that may or may not affect the proper execution of the game.
 	ALERT,
-	## Catastrophic events that stop the execution of the game in its tracks. Use this to inform yourself or the player what went wrong if you make it so the game closes. 
+	## Catastrophic events that stop the execution of the game in its tracks.[br]
+	##Use this to inform yourself or the player what went wrong if you make it so the game closes. 
 	ERROR
 }
 var _LOGSEVERITYDICT : Dictionary = {
@@ -81,33 +88,33 @@ var _LOGSEVERITYDICT : Dictionary = {
 	LOGSEVERITY.ERROR   : "ERROR",
 }
 
-## The logger will save to disk before it gets deleted from memory, but you may disable it if you wish. Remember to use [method save_log] if you disable autosaving
+## The logger will save to disk before it gets deleted from memory, but you may disable it if you wish.[br]
+## Remember to use [method save_log] if you disable autosaving.
 func setAUTOSAVE(value : bool) -> bool:
 	_AUTOSAVEONEXIT = value
 	return _AUTOSAVEONEXIT
 
-## Writes the current log buffer to disk. You don't need to call this method unless you turn off autosave.
+## Writes the current log buffer to disk. You don't need to call this method unless you turn off autosaving.
 func save_log() -> bool:
 	var file = FileAccess.open(_LOGFILESPATH + "latest.log", FileAccess.WRITE_READ)
-	file.store_string(buffer)
+	file.store_string(_buffer)
 	file.close()
+	_buffer = ""
 	return true
 
-## @experimental
+## Adds a log to the buffer. Log text prints the milliseconds since the engine started, the current time, the [param severity] of the log, and the [param contents].
 func add_log(contents : String, severity : int) -> void:
-	buffer = buffer + str(Time.get_ticks_msec()) + "|"
-	buffer = buffer + "[" + Time.get_time_string_from_system() + "]" + "|"
-	buffer = buffer + _LOGSEVERITYDICT.get(severity) + "|"
-	buffer = buffer + contents
-	buffer = buffer + "\n"
+	_buffer = _buffer + str(Time.get_ticks_msec()) + "|"
+	_buffer = _buffer + "[" + Time.get_time_string_from_system() + "]" + "|"
+	_buffer = _buffer + _LOGSEVERITYDICT.get(severity) + "|"
+	_buffer = _buffer + contents
+	_buffer = _buffer + "\n"
 
 func _sortlogs() -> void:
 	var files : PackedStringArray = DirAccess.get_files_at(_LOGFILESPATH)
 	var pathtolatest : String = _LOGFILESPATH + "latest.log"
 	if files.size() > 0 and files.has("latest.log"):
 		var err : Error = DirAccess.rename_absolute(pathtolatest, _LOGFILESPATH + "bonionlog" + str(files.size()) + ".log")
-		print(str(pathtolatest))
-		print(error_string(err))
 	files = DirAccess.get_files_at(_LOGFILESPATH)
 	if files.size() > 4:
 		DirAccess.remove_absolute(_LOGFILESPATH + files[files.size()-1])
