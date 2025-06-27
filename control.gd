8d7a6fc6f50c453cf592d78694449175660fc23d
Bonion-Desktop <55188452+boniondev@users.noreply.github.com>
2025-06-27 10:51:18 +0200

    A way to test it out

--- control.gd
+++ control.gd
@@ -5,6 +5,7 @@ func _ready() -> void:
 	logger.add_log("Testing!",logger.LOGSEVERITY.DEBUG)
 	logger.add_log("Testing!",logger.LOGSEVERITY.INFO)
 	logger.add_log("Testing!",logger.LOGSEVERITY.ERROR)
-	logger.add_log("Testing!",logger.LOGSEVERITY.WARNING)
-	logger.add_log("Testing!",logger.LOGSEVERITY.ALERT)
+	logger.set_logginglevel(logger.LOGSEVERITY.INFO)
+	logger.add_log("This shouldn't appear now!", logger.LOGSEVERITY.DEBUG)
+	logger.add_log("This should!", logger.LOGSEVERITY.INFO)
 	logger = null
